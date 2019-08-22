7238535536dc864a2e81c9267877a964bef54c20
Ben Mayes <czahrien@gmail.com>
2019-08-21 22:07:54 -0400

    Require authentication for the TemperatureDataPoint API.

--- data/views.py
+++ data/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, authentication, permissions
 from .serializers import TemperatureDataPointSerializer
 from .models import TemperatureDataPoint
 
+
 class TemperatureDataPointViewset(viewsets.ModelViewSet):
+    authentication_classes = [authentication.SessionAuthentication,
+                              authentication.BasicAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = TemperatureDataPoint.objects.all().order_by('date')
     serializer_class = TemperatureDataPointSerializer
